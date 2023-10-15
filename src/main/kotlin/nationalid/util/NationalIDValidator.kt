75e40d5aed4ac064456d82e1e7593668b66e4621
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2023-10-15 23:37:53 +0300

    update validator exceptions

--- src/main/kotlin/nationalid/util/NationalIDValidator.kt
+++ src/main/kotlin/nationalid/util/NationalIDValidator.kt
@@ -4,21 +4,27 @@ import nationalid.model.NationalIDFormatException
 
 class NationalIDValidator(private val nationalID: String) : INationalIDValidator {
     override fun validate() {
-        if (nationalID.length != 14)
-            throw NationalIDFormatException("National ID must be 14 numbers")
-        if (containsNonNumericCharacters(nationalID))
-            throw NationalIDFormatException("National ID shouldn't contain any characters")
-        if (nationalID.first().digitToInt() <= 0)
-            throw NationalIDFormatException("National ID should start with 1 or bigger than it")
+        require(nationalID.length == 14) {
+            throw NationalIDFormatException(NATIONAL_ID_LENGTH_EXCEPTION_MSG)
+        }
+        require(nationalID.first().digitToInt() > 0) {
+            throw NationalIDFormatException(NATIONAL_ID_FIRST_NUMBER_EXCEPTION_MSG)
+        }
+        require(containsNumericCharacters(nationalID)) {
+            throw NationalIDFormatException(NATIONAL_ID_REQUIRE_CHARACTERS_EXCEPTION_MSG)
+        }
     }
 
-    private fun containsNonNumericCharacters(input: String): Boolean {
+    private fun containsNumericCharacters(input: String): Boolean {
         val regex = Regex(NATIONAL_ID_PATTERN)
-        return regex.containsMatchIn(input)
+        return regex.containsMatchIn(input).not()
     }
 
 
     companion object {
         private const val NATIONAL_ID_PATTERN = "[^0-9]"
+        private const val NATIONAL_ID_LENGTH_EXCEPTION_MSG = "National ID must be 14 numbers"
+        private const val NATIONAL_ID_FIRST_NUMBER_EXCEPTION_MSG = "National ID should start with 1 or bigger than it"
+        private const val NATIONAL_ID_REQUIRE_CHARACTERS_EXCEPTION_MSG = "National ID shouldn't contain any characters"
     }
 }
